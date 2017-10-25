ea3b23124653b99c851a8e0a9c32d3c956845bf8
kohei0408 <kohei.hamada@yahoo.com>
2017-10-25 15:52:55 +0900

    designation the route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :messages, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:edit, :update]
 end
