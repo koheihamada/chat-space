5bf9f3882f015ad2293a2aec9f0c5435174fa8a9
kohei0408 <kohei.hamada@yahoo.com>
2017-10-27 15:54:52 +0900

    models

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,19 @@ class Group < ApplicationRecord
   has_many :groups_users
   has_many :users, through: :groups_users
   has_many :messages
+
+
+def nanana
+
+    if messages.last.try(:image).blank? && messages.last.try(:body).blank?
+      'なにもありましせん'
+    elsif messages.last.try(:image).blank?
+      messages.last.try(:body)
+    elsif messages.last.try(:body).blank?
+      '写真があります'
+    else
+      '写真があります'
+      end
+  end
+
 end
