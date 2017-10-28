758a60f728d75d2f522014d8a14be9be48601765
kohei0408 <kohei.hamada@yahoo.com>
2017-10-28 14:44:18 +0900

    spec model/meesage

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+describe Message do
+  describe '#create' do
+    it "is valid with a body" do
+      message = build(:message, body: "aa")
+      expect(message).to be_valid
+    end
+
+    it "is valid with a image" do
+      message = build(:message, image: "ccc.jpg")
+      expect(message).to be_valid
+    end
+
+    it "is valid with image & body" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
+    it "is invalid without image nor body" do
+      message = build(:message, image: nil, body: nil)
+      message.valid?
+      expect(message.errors[:body_or_image]).to include( "を入力してください")
+    end
+
+    it "is invalid without a group_id" do
+      message = build(:message, group_id: :nil)
+      message.valid?
+      expect(message.errors[:group]).to include("を入力してください")
+    end
+
+    it "is invalid without a user_id" do
+      message = build(:message, user_id: :nil)
+      message.valid?
+      expect(message.errors[:user]).to include("を入力してください")
+    end
+  end
+
+end
