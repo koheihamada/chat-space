ab0e1fb59569a674e6133a969c01d3c46b48ce4d
kohei0408 <kohei.hamada@yahoo.com>
2017-10-28 16:17:38 +0900

    changed hash data

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,28 @@
 FactoryGirl.define do
-
   factory :message do
-    body {Faker::Lorem.sentence}
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixture/profile.jpg')) }
-    group
-    user
+   factory :onlybody do
+     body {Faker::Lorem.sentence}
+     image ""
+     group
+     user
+    end
+
+    factory :onlyimage do
+     body ""
+     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixture/profile.jpg')) }
+     group
+     user
+    end
+
+
+
+   factory :all do
+     body {Faker::Lorem.sentence}
+     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixture/profile.jpg')) }
+     group
+     user
+   end
   end
+
 
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,35 +3,35 @@ describe Message do
   describe '#create' do
     context 'メッセージを保存できる場合' do
       it "is valid with a body" do
-        message = build(:message)
+        message = build(:onlybody)
         expect(message).to be_valid
       end
 
       it "is valid with a image" do
-        message = build(:message)
+        message = build(:onlyimage)
         expect(message).to be_valid
       end
 
       it "is valid with image & body" do
-        message = build(:message)
+        message = build(:all)
         expect(message).to be_valid
       end
     end
     context 'メッセージを保存できない場合' do
       it "is invalid without image nor body" do
-        message = build(:message, image: nil, body: nil)
+        message = build(:all, image: nil, body: nil)
         message.valid?
         expect(message.errors[:body_or_image]).to include( "を入力してください")
       end
 
       it "is invalid without a group_id" do
-        message = build(:message, group_id: :nil)
+        message = build(:all, group_id: :nil)
         message.valid?
         expect(message.errors[:group]).to include("を入力してください")
       end
 
       it "is invalid without a user_id" do
-        message = build(:message, user_id: :nil)
+        message = build(:all, user_id: :nil)
         message.valid?
         expect(message.errors[:user]).to include("を入力してください")
       end
