eaafa23effe5710c9c08eb0ffa3a3786cae018b2
kohei0408 <kohei.hamada@yahoo.com>
2017-11-01 11:22:51 +0900

    edit picture system

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,20 @@
 $(document).on ('turbolinks:load', function(){
   function buildHTML(message){
     var sample =  message.image !== null ? ` <img alt="${message.image}" src="${message.image}" />` : ""
-    var html = `<div class= "text"><h1>${message.name}
-                </h1>
-                <h2> ${message.created_at}
+    var html = `<div class= "text">
+                  <h1>
+                    ${message.name}
+                  </h1>
+                  <h2>
+                    ${message.created_at}
                   </h2>
                   <div class= "message">
-                    <h3>${message.body}
-                      </h3>
+                    <h3>
+                      ${message.body}
+                    </h3>
                   </div>
-                  ${sample}
-                  </div>`
+                    ${sample}
+                   </div>`
                   return html
   }
 
@@ -31,11 +35,12 @@ $(document).on ('turbolinks:load', function(){
       $('.right-content__middle').append(html)
       $('.send').attr('disabled', false);
       $('#message_body').val('')
+      $('#message_image').val('')
       $('.right-content__middle').animate({scrollTop: $('.right-content__middle')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
       alert('errorrr');
-      // $('.send').attr('disabled', false);
+      $('.send').attr('disabled', false);
     })
    });
 });
