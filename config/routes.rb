5fd19f3e0237aab84d62529dd3c2cd334023e69c
kohei0408 <kohei.hamada@yahoo.com>
2017-11-02 13:07:51 +0900

    edit routing for edit group

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'groups#index'
   devise_for :users
-  resources :groups, only: [:new, :create, :index, :edit] do
+  resources :groups, only: [:new, :create, :index, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
 end
